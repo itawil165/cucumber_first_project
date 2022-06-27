d05d302dacdcf5fbcf3100f08347a03a4ada2916
Ismail Tawil <itawil165@gmail.com>
2022-06-27 01:56:05 -0500

    Initial commit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,19 @@
+package runners;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "steps",
+        tags = "@Regression",
+        plugin = {"html:target/cucumber-reports",
+                "json:target/cucumber-reports/Cucumber.json",
+                "pretty"
+        }
+)
+
+public class Runner {
+
+}
